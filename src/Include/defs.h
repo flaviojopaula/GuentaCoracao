dc091289bb6e21a0308c72791d624e5bfe87c20d
Eduardo Moscatelli de Souza <moscatelli.eduardo@gmail.com>
2014-04-29 21:38:56 -0300

    Arquivo src/Include/defs.h criado

---
+++ src/Include/defs.h
@@ -0,0 +1,15 @@
+#ifndef DEFS_H_
+#define DEFS_H_
+
+#define INT int
+#define UINT unsigned int
+#define CHAR char
+#define UCHAR unsigned char
+#define SHORT short
+#define USHORT unsigned short
+#define LONG long
+#define ULONG unsigned long
+#define FLOAT float
+#define DOUBLE double
+
+#endif
